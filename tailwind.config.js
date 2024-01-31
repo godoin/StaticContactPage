7347ae2ffa5154628dd217ae468032d5263695e5
Godwin Duliente <rualgodwin@gmail.com>
2024-01-31 11:48:10 +0800

    added custom hovers to buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,14 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.custom-shadow': {
+          boxShadow: '0.3rem 0.3rem 0rem rgb(0, 0, 0)',
+        },
+      };
+      addUtilities(newUtilities, ['hover']);
+    },
+  ],
 };
